118b49ee07770290ca96ea01efd14cbc00b1f631
jamesjaa <144088647+jamesjaa@users.noreply.github.com>
2024-04-05 00:25:30 +0700

    Update index.blade.php

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -22,7 +22,6 @@
     <script>
         $(document).ready(function() {
             $("#myTable").DataTable();
-            $("#myTable2").DataTable();
         });
     </script>
 
@@ -49,7 +48,7 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
         <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center">
+            <a href="/api/dashboard" class="logo d-flex align-items-center">
                 <!-- <img src="assets/img/logo.png" alt=""> -->
                 <span class="d-none d-lg-block">DATA CENTER</span>
             </a>
